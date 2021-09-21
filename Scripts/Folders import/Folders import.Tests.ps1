2c5de6d9150a482418e9f8e94376db19b59bcb57
DarkLite1 <BrechtGijbels@gmail.com>
2021-09-21 11:42:56 +0200

    added tests for folder creation

---
+++ Scripts/Folders import/Folders import.Tests.ps1
@@ -0,0 +1,98 @@
+#Requires -Modules Pester
+#Requires -Version 5.1
+
+BeforeAll {
+    $testFolders = @(
+        (New-Item 'TestDrive:/B/B1/B11' -ItemType Directory).FullName,
+        (New-Item 'TestDrive:/C/C1' -ItemType Directory).FullName
+    )
+    $testScript = $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+    $testParams = @{
+        Action          = 'Import'
+        DataFolder      = (New-Item 'TestDrive:/A' -ItemType Directory).FullName
+        foldersFileName = 'folders.txt'
+    }
+
+    Mock Write-Output
+}
+AfterAll {
+    $testFolders | Remove-Item
+}
+Describe 'the mandatory parameters are' {
+    It '<_>' -ForEach 'Action', 'DataFolder' {
+        (Get-Command $testScript).Parameters[$_].Attributes.Mandatory | 
+        Should -BeTrue
+    }
+}
+Describe 'Fail the import of Folders when' {
+    BeforeEach {
+        Get-ChildItem $testParams.DataFolder | Remove-Item
+        $testNewParams = $testParams.clone()
+    }
+    It 'the data folder is not found' {
+        $testNewParams.DataFolder = 'TestDrive:/xxx'
+
+        { .$testScript @testNewParams } | 
+        Should -Throw "*Import folder 'TestDrive:/xxx' not found"
+    }
+    It 'the data folder is empty' {
+        { .$testScript @testNewParams } | 
+        Should -Throw "*Import folder '$($testNewParams.DataFolder)' empty"
+    }
+    It 'the data folder does not have the folders file' {
+        '1' | Out-File -LiteralPath "$($testNewParams.DataFolder)\file.txt"
+
+        { .$testScript @testNewParams } | 
+        Should -Throw "*Folders file '$($testNewParams.DataFolder)\$($testNewParams.foldersFileName)' not found"
+    }
+}
+Describe "With Action set to 'Import'" {
+    BeforeAll {
+        $testJoinParams = @{
+            Path      = $testParams.DataFolder
+            ChildPath = $testParams.foldersFileName
+        }
+        $testFoldersFile = Join-Path @testJoinParams
+        $testFolders | Out-File -FilePath $testFoldersFile
+    }
+    It 'folders are created when they do not exist' {
+        $testFolders | Remove-Item
+
+        .$testScript @testParams
+
+        foreach ($testFolder in $testFolders) {
+            $testFolder | Should -Exist
+            
+            Should -Invoke Write-Output -ParameterFilter {
+                $InputObject -eq "Folder '$testFolder' created"
+            }
+        }
+    }
+    It 'folders are left alone when they already exist' {
+        foreach ($testFolder in $testFolders) {
+            New-Item -Path $testFolder -ItemType Directory -EA Ignore
+        }
+
+        .$testScript @testParams
+
+        foreach ($testFolder in $testFolders) {
+            $testFolder | Should -Exist
+
+            Should -Invoke Write-Output -ParameterFilter {
+                $InputObject -eq "Folder '$testFolder' exists already"
+            }
+        }
+    }
+    Context 'non terminating errors are generated when' {
+        It 'a folder cannot be created' {
+            'wrong' | Out-File -FilePath $testFoldersFile
+
+            $Error.Clear()
+            .$testScript @testParams -EA SilentlyContinue
+
+            $Error.Exception.Message | Where-Object {
+                $_ -like "*Failed to create folder 'wrong': Path not valid"
+            } | Should -Not -BeNullOrEmpty
+        }
+    }
+}
---
+++ Scripts/Folders import/Folders import.ps1
@@ -0,0 +1,96 @@
+<#
+    .SYNOPSIS
+        Create folders.
+
+    .DESCRIPTION
+        Read the folder paths from an input file and create the required 
+        folders. This script will never delete a folder.
+        
+    .PARAMETER Action
+        When action is 'Import' the required folders will be created.
+
+    .PARAMETER DataFolder
+        Folder where file can be found that contains the folder paths.
+
+    .EXAMPLE
+        $exportParams = @{
+            Action     = 'Export'
+            DataFolder = 'C:\SmbShares'
+        }
+        & 'C:\ImportExportSmbShares.ps1' @exportParams
+
+        Export all Folders on the current computer to the folder 'SmbShares'
+
+    .EXAMPLE
+        $importParams = @{
+            Action     = 'Import'
+            DataFolder = 'C:\SmbShares'
+        }
+        & 'C:\ImportExportSmbShares.ps1' @importParams
+
+        Import all Folders in the folder 'SmbShares'
+#>
+
+[CmdletBinding()]
+Param(
+    [ValidateSet('Import')]
+    [Parameter(Mandatory)]
+    [String]$Action,
+    [Parameter(Mandatory)]
+    [String]$DataFolder,
+    [String]$foldersFileName = 'Folders.txt'
+)
+
+Begin {    
+    Try {
+        $foldersFile = Join-Path -Path $DataFolder -ChildPath $foldersFileName
+
+        #region Test DataFolder
+        If (-not (Test-Path -LiteralPath $DataFolder -PathType Container)) {
+            throw "Import folder '$DataFolder' not found"
+        }
+        If ((Get-ChildItem -Path $DataFolder | Measure-Object).Count -eq 0) {
+            throw "Import folder '$DataFolder' empty"
+        }
+        If (-not (Test-Path -LiteralPath $foldersFile -PathType Leaf)) {
+            throw "Folders file '$foldersFile' not found"
+        }
+        #endregion
+    }
+    Catch {
+        throw "$Action folders failed: $_"
+    }
+}
+
+Process {
+    Try {
+        Write-Verbose "Import folders from file '$foldersFile'"
+        $folders = Get-Content -LiteralPath $foldersFile -Encoding UTF8
+        
+        foreach ($folder in $folders) {
+            Try {
+                Write-Verbose "Folder '$folder'"
+          
+                #region Create Folder
+                If (Test-Path -LiteralPath $folder -PathType Container) {
+                    Write-Output "Folder '$folder' exists already"
+                }
+                else {
+                    if ($folder -NotMatch '^([a-zA-Z]+:)?(\\[a-zA-Z0-9-_.-: :]+)*\\?$') {
+                        throw "Path not valid"
+                    }
+                    $null = New-Item -Path $folder -ItemType Directory
+                    Write-Output "Folder '$folder' created"
+                }
+                #endregion
+            }
+            Catch {
+                Write-Error "Failed to create folder '$folder': $_"
+                $Error.RemoveAt(1)
+            }
+        }
+    }
+    Catch {
+        throw "$Action Folders failed: $_"
+    }
+}
